3e9ac612f673a4f2e50599372d465f0700eb07e9
isaia <isaiahnoelsalazar474@gmail.com>
2024-04-01 21:45:46 +0800

    Almond updated to 1.0.8

--- customs/src/main/java/com/salazarisaiahnoel/customs/Fullscreen.java
+++ customs/src/main/java/com/salazarisaiahnoel/customs/Fullscreen.java
@@ -2,10 +2,18 @@ package com.salazarisaiahnoel.customs;
 
 import android.app.Activity;
 import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class Fullscreen {
     public static void enableFullscreen(Activity activity){
-        int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        activity.getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+        activity.getWindow().getDecorView().setSystemUiVisibility(WindowManager.LayoutParams.FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        Objects.requireNonNull(activity.getActionBar()).hide();
+    }
+
+    public static void disableFullscreen(Activity activity){
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.VISIBLE);
+        Objects.requireNonNull(activity.getActionBar()).show();
     }
 }
